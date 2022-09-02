41e6f247e7865e21481df81fbf5aff38ec002ae2
Dyaha1403 <an636646@gmail.com>
2022-09-02 23:45:58 +0300

    First version

---
+++ Horoskope/Class_Library_Parse/Parser.cs
@@ -0,0 +1,33 @@
+﻿using HtmlAgilityPack;
+
+namespace Class_Library_For_Pase
+{
+    public class Parser
+    {
+        string[] zodiac_sign = new string[] { "aries", "taurus", "gemini",
+                                              "cancer", "leo", "virgo",
+                                              "libra", "scorpio", "sagittarius",
+                                              "capricorn", "aquarius", "pisces"};
+        public string[] ParsHoroscope(int num_zodiack)
+        {
+            string[] text = new string[2];
+
+            string url = $"https://horoscopes.rambler.ru/{zodiac_sign[num_zodiack]}/";
+
+            HtmlWeb webDoc = new HtmlWeb();
+
+            HtmlDocument doc = webDoc.Load(url);
+            HtmlNode par = doc.DocumentNode.SelectSingleNode("//h1[@class='_3wtsS _1W8Ro _2kQY7']");
+
+            text[0] = par.InnerText;
+
+            par = doc.DocumentNode.SelectSingleNode("//p[@class='mtZOt']");
+
+            text[1] = par.InnerText;
+
+            return text;
+        }
+
+
+    }
+}
---
+++ Horoskope/Rambler_Horoscope_App/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Class_Library_For_Pase;
+
+namespace Rambler_Horoscope_App
+{
+    public partial class MainWindow : Window
+    {
+
+        Parser parser = new Parser();
+
+        string[] title = new string[12];
+        string[] horoscope = new string[12];
+        public MainWindow() 
+        {
+            InitializeComponent();
+            InitializeParsMethod();
+        }
+
+        private void InitializeParsMethod()
+        {
+            for(int i = 0; i < 12; i++)
+            {
+                title[i] = parser.ParsHoroscope(i)[0];
+                horoscope[i] = parser.ParsHoroscope(i)[1];
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+                DragMove(); 
+        }
+
+       private void ButtonMinimazed_Click(object sender, RoutedEventArgs e)
+       {
+           this.WindowState = WindowState.Minimized;
+       }
+
+        private void CloseWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonAries_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[0];
+
+            HoroscopeText.Text = horoscope[0];
+        }
+
+        private void ButtonTaurus_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[1];
+
+            HoroscopeText.Text = horoscope[1];
+        }
+
+        private void ButtonGemini_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[2];
+
+            HoroscopeText.Text = horoscope[2];
+        }
+
+        private void ButtonCancer_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[3];
+
+            HoroscopeText.Text = horoscope[3];
+        }
+
+        private void ButtonLeo_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[4];
+
+            HoroscopeText.Text = horoscope[4];
+        }
+
+        private void ButtonVirgo_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[5];
+
+            HoroscopeText.Text = horoscope[5];
+        }
+
+        private void ButtonLibra_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[6];
+
+            HoroscopeText.Text = horoscope[6];
+        }
+
+        private void ButtonScorpio_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[7];
+
+            HoroscopeText.Text = horoscope[7];
+        }
+
+        private void ButtonSagittar_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[8];
+
+            HoroscopeText.Text = horoscope[8];
+        }
+
+        private void ButtonCaprico_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[9];
+
+            HoroscopeText.Text = horoscope[9];
+        }
+
+        private void ButtonAquarius_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[10];
+
+            HoroscopeText.Text = horoscope[10];
+        }
+
+        private void ButtonPisces_Click(object sender, RoutedEventArgs e)
+        {
+            TitleText.Text = title[11];
+
+            HoroscopeText.Text = horoscope[11];
+        }
+    }
+}
